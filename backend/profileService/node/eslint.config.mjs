f24d24887be944c0a83fb2c0a93d0ece6c0d54c9
KStrzechowski <strzechowski.konrad@gmail.com>
2024-09-19 02:14:42 +0200

    Update packages

---
+++ backend/profileService/node/eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    eslintPluginPrettierRecommended,
+    {
+      ignores: ['node_modules', 'build'],
+    }
+];
