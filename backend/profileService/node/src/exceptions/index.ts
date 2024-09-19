cd90f17772ffc8d1a9c0551121d7c8d4faccb0b9
KStrzechowski <strzechowski.konrad@gmail.com>
2024-09-19 02:24:06 +0200

    Add general exceptions

---
+++ backend/profileService/node/src/exceptions/index.ts
@@ -0,0 +1,5 @@
+import HttpException from "./HttpException";
+export { ForbiddenException } from "./ForbiddenException";
+export { NotFoundException } from "./NotFoundException";
+
+export { HttpException };
